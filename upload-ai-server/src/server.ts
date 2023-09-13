c84006ea2520112a3b30c417e63d54b72b57d001
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2023-09-12 23:49:29 -0300

    feat: video completion route

--- upload-ai-server/src/server.ts
+++ upload-ai-server/src/server.ts
@@ -2,12 +2,18 @@ import { fastify } from 'fastify'
 import { getAllPromptsRoute } from './routes/get-all-prompts'
 import { uploadVideoRoute } from './routes/upload-video'
 import { createTranscriptionRoute } from './routes/create-transcription'
+import { generateAICompletionRoute } from './routes/generate-ai-completion'
+import { fastifyCors } from '@fastify/cors'
 
 const app = fastify()
+app.register(fastifyCors, {
+  origin: '*',
+})
 
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
 app.register(createTranscriptionRoute)
+app.register(generateAICompletionRoute)
 
 app.get('/', () => {
   return 'API online'
